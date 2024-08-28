a1b918b18258595b0f24f7cb517bd56d644fbc35
Ibrahim Gabr <128988435+ibrahimGabr977@users.noreply.github.com>
2024-08-28 15:06:49 +0300

    Delete src directory

--- src/main/java/com/igb/web/online_library/repositories/PatronRepository.java
+++
@@ -1,9 +0,0 @@
-package com.igb.web.online_library.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.igb.web.online_library.model.Patron;
-
-public interface  PatronRepository extends CrudRepository<Patron, Long> {
-
-}
